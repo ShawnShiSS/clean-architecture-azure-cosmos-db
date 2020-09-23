901cad806322a1cbccfb1afa5e82d7d8bac2e5ad
Shawn Shi <ss.shawnshi@gmail.com>
2020-09-22 23:34:38 -0400

    Infrastructure - Implement CosmosDB client abstraction

---
+++ src/CleanArchitectureCosmosDB.Infrastructure/CosmosDbData/CosmosDbContainer.cs
@@ -0,0 +1,17 @@
+﻿using CleanArchitectureCosmosDB.Infrastructure.CosmosDbData.Interfaces;
+using Microsoft.Azure.Cosmos;
+
+namespace CleanArchitectureCosmosDB.Infrastructure.CosmosDbData
+{
+    public class CosmosDbContainer : ICosmosDbContainer
+    {
+        public Container _container { get; }
+
+        public CosmosDbContainer(CosmosClient cosmosClient,
+                                 string databaseName,
+                                 string containerName)
+        {
+            this._container = cosmosClient.GetContainer(databaseName, containerName);
+        }
+    }
+}
---
+++ src/CleanArchitectureCosmosDB.Infrastructure/CosmosDbData/CosmosDbContainerFactory.cs
@@ -0,0 +1,46 @@
+﻿using CleanArchitectureCosmosDB.Infrastructure.CosmosDbData.Config;
+using CleanArchitectureCosmosDB.Infrastructure.CosmosDbData.Interfaces;
+using Microsoft.Azure.Cosmos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CleanArchitectureCosmosDB.Infrastructure.CosmosDbData
+{
+    public class CosmosDbContainerFactory : ICosmosDbContainerFactory
+    {
+        private readonly CosmosClient _cosmosClient;
+        private readonly string _databaseName;
+        private readonly List<ContainerInfo> _containers;
+
+        public CosmosDbContainerFactory(CosmosClient cosmosClient,
+                                   string databaseName,
+                                   List<ContainerInfo> containers)
+        {
+            _databaseName = databaseName ?? throw new ArgumentNullException(nameof(databaseName));
+            _containers = containers ?? throw new ArgumentNullException(nameof(containers));
+            _cosmosClient = cosmosClient ?? throw new ArgumentNullException(nameof(cosmosClient));
+        }
+
+        public ICosmosDbContainer GetContainer(string containerName)
+        {
+            if (_containers.Where(x => x.Name == containerName) == null)
+            {
+                throw new ArgumentException($"Unable to find container: {containerName}");
+            }
+
+            return new CosmosDbContainer(_cosmosClient, _databaseName, containerName);
+        }
+
+        public async Task EnsureDbSetupAsync()
+        {
+            Microsoft.Azure.Cosmos.DatabaseResponse database = await _cosmosClient.CreateDatabaseIfNotExistsAsync(_databaseName);
+
+            foreach (var container in _containers)
+            {
+                await database.Database.CreateContainerIfNotExistsAsync(container.Name, $"{container.PartitionKey}");
+            }
+        }
+    }
+}
