c1a772cfbe1783c06b185d22fed6740243696096
Shawn Shi <ss.shawnshi@gmail.com>
2020-09-23 21:22:30 -0400

    Infrastructure - Implement CosmosDbReposiroty and ToDoItemRepository

---
+++ src/CleanArchitectureCosmosDB.Infrastructure/CosmosDbData/Repository/CosmosDbRepository.cs
@@ -0,0 +1,84 @@
+﻿using CleanArchitectureCosmosDB.Core.Entities.Base;
+using CleanArchitectureCosmosDB.Core.Interfaces;
+using CleanArchitectureCosmosDB.Infrastructure.CosmosDbData.Interfaces;
+using Microsoft.Azure.Cosmos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CleanArchitectureCosmosDB.Infrastructure.CosmosDbData.Repository
+{
+    public abstract class CosmosDbRepository<T> : IRepository<T>, IContainerContext<T> where T : BaseEntity
+    {
+        /// <summary>
+        ///     Name of the CosmosDB container
+        /// </summary>
+        public abstract string ContainerName { get; }
+
+        /// <summary>
+        ///     Generate id
+        /// </summary>
+        /// <param name="entity"></param>
+        /// <returns></returns>
+        public abstract string GenerateId(T entity);
+
+        /// <summary>
+        ///     Resolve the partition key
+        /// </summary>
+        /// <param name="entityId"></param>
+        /// <returns></returns>
+        public abstract PartitionKey ResolvePartitionKey(string entityId);
+
+        private readonly ICosmosDbContainerFactory _cosmosDbContainerFactory;
+        private readonly Microsoft.Azure.Cosmos.Container _container;
+        public CosmosDbRepository(ICosmosDbContainerFactory cosmosDbContainerFactory)
+        {
+            this._cosmosDbContainerFactory = cosmosDbContainerFactory ?? throw new ArgumentNullException(nameof(ICosmosDbContainerFactory));
+            this._container = this._cosmosDbContainerFactory.GetContainer(ContainerName)._container;
+        }
+
+        public async Task AddItemAsync(T item)
+        {
+            item.Id = GenerateId(item);
+            await _container.CreateItemAsync<T>(item, ResolvePartitionKey(item.Id));
+        }
+
+        public async Task DeleteItemAsync(string id)
+        {
+            await this._container.DeleteItemAsync<T>(id, ResolvePartitionKey(id));
+        }
+
+        public async Task<T> GetItemAsync(string id)
+        {
+            try
+            {
+                ItemResponse<T> response = await _container.ReadItemAsync<T>(id, ResolvePartitionKey(id));
+                return response.Resource;
+            }
+            catch (CosmosException ex) when (ex.StatusCode == System.Net.HttpStatusCode.NotFound)
+            {
+                return null;
+            }
+        }
+
+        public async Task<IEnumerable<T>> GetItemsAsync(string queryString)
+        {
+            var query = _container.GetItemQueryIterator<T>(new QueryDefinition(queryString));
+            List<T> results = new List<T>();
+            while (query.HasMoreResults)
+            {
+                var response = await query.ReadNextAsync();
+
+                results.AddRange(response.ToList());
+            }
+
+            return results;
+        }
+
+        public async Task UpdateItemAsync(string id, T item)
+        {
+            await this._container.UpsertItemAsync<T>(item, ResolvePartitionKey(id));
+        }
+    }
+}
---
+++ src/CleanArchitectureCosmosDB.Infrastructure/CosmosDbData/Repository/ToDoItemRepository.cs
@@ -0,0 +1,38 @@
+﻿using CleanArchitectureCosmosDB.Core.Entities;
+using CleanArchitectureCosmosDB.Core.Interfaces;
+using CleanArchitectureCosmosDB.Infrastructure.CosmosDbData.Interfaces;
+using Microsoft.Azure.Cosmos;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CleanArchitectureCosmosDB.Infrastructure.CosmosDbData.Repository
+{
+    public class ToDoItemRepository : CosmosDbRepository<ToDoItem>, IToDoItemRepository
+    {
+        /// <summary>
+        ///     CosmosDB container name
+        /// </summary>
+        public override string ContainerName { get; } = "todos";
+
+        /// <summary>
+        ///     Generate Id.
+        ///     e.g. "shoppinglist:783dfe25-7ece-4f0b-885e-c0ea72135942"
+        /// </summary>
+        /// <param name="entity"></param>
+        /// <returns></returns>
+        public override string GenerateId(ToDoItem entity) => $"{entity.Category}:{Guid.NewGuid()}";
+
+        /// <summary>
+        ///     Returns the value of the partition key
+        /// </summary>
+        /// <param name="entityId"></param>
+        /// <returns></returns>
+        public override PartitionKey ResolvePartitionKey(string entityId) => new PartitionKey(entityId.Split(':')[0]);
+
+        public ToDoItemRepository(ICosmosDbContainerFactory factory) : base(factory)
+        { }
+
+
+    }
+}
