4cf3b788ce763eecc6d0821026d29061561e4692
Shawn Shi <ss.shawnshi@gmail.com>
2020-09-23 21:26:49 -0400

    Infrastructure - add extension method to register CosmosClient wrapper

---
+++ src/CleanArchitectureCosmosDB.Infrastructure/CosmosDbData/Extensions/IServiceCollectionCosmosDbExtensions.cs
@@ -0,0 +1,37 @@
+﻿using CleanArchitectureCosmosDB.Infrastructure.CosmosDbData.Config;
+using CleanArchitectureCosmosDB.Infrastructure.CosmosDbData.Interfaces;
+using Microsoft.Extensions.DependencyInjection;
+using System.Collections.Generic;
+
+namespace CleanArchitectureCosmosDB.Infrastructure.CosmosDbData.Extensions
+{
+    public static class IServiceCollectionCosmosDbExtensions
+    {
+        /// <summary>
+        ///     Register a singleton instance of Cosmos Db Container Factory, which is a wrapper for the CosmosClient.
+        /// </summary>
+        /// <param name="services"></param>
+        /// <param name="endpointUrl"></param>
+        /// <param name="primaryKey"></param>
+        /// <param name="databaseName"></param>
+        /// <param name="containers"></param>
+        /// <returns></returns>
+        public static IServiceCollection AddCosmosDb(this IServiceCollection services,
+                                                     string endpointUrl,
+                                                     string primaryKey,
+                                                     string databaseName,
+                                                     List<ContainerInfo> containers)
+        {
+            Microsoft.Azure.Cosmos.CosmosClient client = new Microsoft.Azure.Cosmos.CosmosClient(endpointUrl, primaryKey);
+            var cosmosDbClientFactory = new CosmosDbContainerFactory(client, databaseName, containers);
+
+            // Ensure CosmosDB is created
+            cosmosDbClientFactory.EnsureDbSetupAsync().Wait();
+
+            services.AddSingleton<ICosmosDbContainerFactory>(cosmosDbClientFactory);
+
+            return services;
+        }
+
+    }
+}
