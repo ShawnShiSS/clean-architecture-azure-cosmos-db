63d38d4370a2cc2f4db826b85467454699e00d4b
Shawn Shi <ss.shawnshi@gmail.com>
2020-12-18 22:25:38 -0500

    Core: Add storage service interface

---
+++ src/CleanArchitectureCosmosDB.Core/Interfaces/Storage/IStorageService.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Http;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace ApplicationCore.Interfaces.Storage
+{
+    public interface IStorageService
+    {
+        /// <summary>
+        ///     Upload a file and returns the full path to retrieve the file
+        /// </summary>
+        /// <param name="file"></param>
+        /// <returns></returns>
+        Task<string> UploadFile(IFormFile file);
+
+        /// <summary>
+        ///     Get the file stream by full path
+        /// </summary>
+        /// <param name="filePath"></param>
+        /// <returns></returns>
+        Task<Stream> GetFileStream(string filePath);
+    }
+}
