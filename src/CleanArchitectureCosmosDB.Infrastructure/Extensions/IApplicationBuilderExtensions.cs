6a3e310a7917caabdb63d84cd4e531ec5cb9c7e3
Shawn Shi <ss.shawnshi@gmail.com>
2021-04-02 21:18:27 -0400

    Infrastructure: move extension files to the project level

--- src/CleanArchitectureCosmosDB.Infrastructure/Extensions/IApplicationBuilderExtensions.cs
+++ src/CleanArchitectureCosmosDB.Infrastructure/Extensions/IApplicationBuilderExtensions.cs
@@ -7,7 +7,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace CleanArchitectureCosmosDB.Infrastructure.CosmosDbData.Extensions
+namespace CleanArchitectureCosmosDB.Infrastructure.Extensions
 {
     /// <summary>
     ///     IApplicationBuilderExtensions 
