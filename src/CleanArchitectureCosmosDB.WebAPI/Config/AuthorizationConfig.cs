89890ef9650f38e54e11c2ea29f04e71040ba89d
Shawn Shi <ss.shawnshi@gmail.com>
2021-04-04 21:15:27 -0400

    Extension methnoid for authorization configuration

---
+++ src/CleanArchitectureCosmosDB.WebAPI/Config/AuthorizationConfig.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace CleanArchitectureCosmosDB.WebAPI.Config
+{
+    /// <summary>
+    ///     Configure authorization
+    /// </summary>
+    public static class AuthorizationConfig
+    {
+        /// <summary>
+        ///     Authorization
+        /// </summary>
+        /// <param name="services"></param>
+        public static void SetAuthorization(this IServiceCollection services)
+        {
+            services.AddAuthorization(
+                options =>
+                {
+                    options.AddPolicy(
+                        JwtBearerDefaults.AuthenticationScheme,
+                        new AuthorizationPolicyBuilder()
+                            .AddAuthenticationSchemes(JwtBearerDefaults.AuthenticationScheme)
+                            .RequireAuthenticatedUser()
+                            .Build());
+                });
+        }
+    }
+}
