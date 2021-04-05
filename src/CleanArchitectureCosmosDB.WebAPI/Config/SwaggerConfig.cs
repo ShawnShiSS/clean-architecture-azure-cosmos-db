ed4ba79cc0bd5d9e2813629bdba44317c3e3dc5d
Shawn Shi <ss.shawnshi@gmail.com>
2021-04-04 21:16:12 -0400

    Extension method for Swagger configuration

---
+++ src/CleanArchitectureCosmosDB.WebAPI/Config/SwaggerConfig.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using ZymLabs.NSwag.FluentValidation;
+
+namespace CleanArchitectureCosmosDB.WebAPI.Config
+{
+    /// <summary>
+    ///     Swagger
+    /// </summary>
+    public static class SwaggerConfig
+    {
+        /// <summary>
+        ///     NSwag for swagger 
+        /// </summary>
+        /// <param name="services"></param>
+        public static void SetupNSwag(this IServiceCollection services)
+        {
+            // Add the FluentValidationSchemaProcessor as a singleton
+            services.AddSingleton<FluentValidationSchemaProcessor>();
+            services.AddOpenApiDocument((options, serviceProvider) =>
+            {
+                options.DocumentName = "v1";
+                options.Title = "Clean Architecture Cosmos DB API";
+                options.Version = "v1";
+                FluentValidationSchemaProcessor fluentValidationSchemaProcessor = serviceProvider.GetService<FluentValidationSchemaProcessor>();
+                // Add the fluent validations schema processor
+                options.SchemaProcessors.Add(fluentValidationSchemaProcessor);
+
+            });
+
+
+        }
+    }
+}
